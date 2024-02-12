006e83401baab80e4c716d6b531f6b895abfe13d
Kazantsev Artem <me@lokach.ru>
2024-02-12 22:38:15 +0300

    add: main page with questions and page for details

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='questions'),
+    path('<int:question_id>/', views.detail, name='questions_detail'),
+    path('<int:question_id>/vote', views.vote, name='questions_vote'),
 ]
